86b28e8691bb26dc1183838439ac52076c75e257
artygo8 <arthurgossuin@gmail.com>
2024-12-07 12:23:59 +0100

    feat: d07

---
+++ d07/aoc2024d07b.py
@@ -0,0 +1,36 @@
+import sys
+from typing import NamedTuple
+from operator import add, mul
+
+
+class Equation(NamedTuple):
+    total: int
+    numbers: tuple[int, ...]
+
+
+def parse_line(line) -> Equation:
+    total, _op, numbers = line.partition(": ")
+    return Equation(int(total), tuple(map(int, numbers.split())))
+
+
+def concat(a: int, b: int):
+    return int(str(a) + str(b))
+
+
+def possible_values(numbers: tuple[int, ...]):
+    values = set(numbers[:1])
+    for n in numbers[1:]:
+        values = {operator(v, n) for v in values for operator in (add, mul, concat)}
+    return values
+
+
+def main():
+    equations = [parse_line(line) for line in sys.stdin if line.strip()]
+    solution = sum(
+        eq.total for eq in equations if eq.total in possible_values(eq.numbers)
+    )
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
